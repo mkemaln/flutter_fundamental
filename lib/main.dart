182113ea18214b42ee7dbd50c604006e3dd6feb0
mkemaln <kemalmuhammad2509@gmail.com>
2024-10-08 11:17:00 +0700

    add: praktikum 4

--- lib/main.dart
+++ lib/main.dart
@@ -81,8 +81,18 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
-          children: [titleSection, buttonSection, textSection],
+        body: ListView(
+          children: [
+            Image.asset(
+              'panderman.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
         ),
       ),
     );
